0692c6a8f7178b033f90b126fb1afd429c565623
Grady253 <grady.andre2@gmail.com>
2022-10-30 17:20:36 -0700

    finished my thought model.

--- models/user.js
+++ models/user.js
@@ -1,30 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-    {
-       username:{
-           type:String,
-           unique:true,
-           required:true,
-           trimmed:true,
-       },
-       email:{
-           type:String,
-           required:true,
-           unique:true,
-           match: /.+\@.+\..+/,
-       },
-       thoughts: {
-           type:Schema.thought.id,
-           ref: 'thought'
-       },
-       friends:{
-           type: Schema.user.id,
-           ref: 'user'
-       },
-    }
-)
-
-userSchema.virtual("friendCount").get(()=>{
-    return this.friends.length
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  thoughts: {
+    type: Schema.Types.ObjectId,
+    ref: "thought",
+  },
+  friends: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  toJSON: { getters: true },
+  id: false,
 });
+
+userSchema.virtual("friendCount").get(() => {
+  return this.friends.length;
+});
+
+const user = model("user", userSchema);
+
+module.exports = user;
