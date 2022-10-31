6b030a2969595a79167dc33e4a6ad77a9942dcf7
Grady253 <grady.andre2@gmail.com>
2022-10-30 20:18:24 -0700

    added User and thought route folder.

--- models/thought.js
+++ models/thought.js
@@ -1,53 +1,49 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type:String,
-            requird:true,
-            maxlength:280,
-            minlength:1
-        },
-        createdAt: {
-            type:Date,
-            default: Date.now,
-            get:newDate = moment(newDate).format("MM DD, YYYY")
-        },
-        username: {
-            type:String,
-            require: true
-        },
-        reactions: [reactionSchema]
-    }
-)
-
-const reactionSchema = new Schema(
-    {
-        reactionId:{
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody:{
-            type:String,
-            require: true,
-            maxlength:280
-        },
-        username:{
-            type:String,
-            require:true
-        },
-        createdAt: {
-            type:date,
-            default: Date.now,
-            get:newDate = moment(newDate).format("MM DD, YYYY")
-        }
-    }
-)
-
-thoughtSchema.virtual("reactionCount").get(()=>{
-    return this.reactions.length
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    requird: true,
+    maxlength: 280,
+    minlength: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (newDate = moment(newDate).format("MM DD, YYYY")),
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  reactions: [reactionSchema],
 });
 
-const thought = model('thought', thoughtSchema);
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    require: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: date,
+    default: Date.now,
+    get: (newDate = moment(newDate).format("MM DD, YYYY")),
+  },
+});
+
+thoughtSchema.virtual("reactionCount").get(() => {
+  return this.reactions.length;
+});
+
+const thought = model("thought", thoughtSchema);
 
 module.exports = thought;
